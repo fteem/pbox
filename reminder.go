55ffad3ecb9aaaaaa998b2a6af2d50c8d22049c6
Ilija Eftimov <ileeftimov@gmail.com>
2018-01-21 22:02:30 +0100

    Remove unneeded attribute from Reminder struct

--- reminder.go
+++ reminder.go
@@ -1,30 +1,9 @@
 package pillbox
 
-import (
-	"time"
-)
-
 type Reminder struct {
-	ID        int       `json:"id"`
-	Body      string    `json:"body"`
-	Morning   bool      `json:"morning"`
-	Afternoon bool      `json:"afternoon"`
-	Evening   bool      `json:"evening"`
-	LastRan   time.Time `json:"last_ran"`
-}
-
-func (r Reminder) String() string {
-	out := ""
-	out += r.Body + " to be had at: "
-	if r.Morning {
-		out += "morning "
-	}
-	if r.Afternoon {
-		out += "afternoon "
-	}
-	if r.Evening {
-		out += "evening"
-	}
-
-	return out
+	ID        int    `json:"id"`
+	Body      string `json:"body"`
+	Morning   bool   `json:"morning"`
+	Afternoon bool   `json:"afternoon"`
+	Evening   bool   `json:"evening"`
 }
